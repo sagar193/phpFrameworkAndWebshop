03759f22cf6eaa2538327aa1246ff1263f2352fe
rjong14 <rjong14@student.avans.nl>
2019-01-15 00:41:13 +0100

    admin header-footer

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,7 @@
 
-</div>
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/public/js/bootstrap.min.js"></script>
+	</div>	<!--/.main-->
+	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- views/header.php
+++ views/header.php
@@ -1,33 +1,78 @@
+<!DOCTYPE html>
 <html>
-    <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Lumino - Dashboard</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/public/css/bootstrap.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="/public/js/jquery.js"></script>
-    
-    <title> Webshop </title>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="/public/css/admin.css" rel="stylesheet">
 
 </head>
+
 <body>
-<div id="header">
-<?php foreach ($this->header as $key) { ?>
-    <a href=" <?php echo ROOTURL. $key->MenuLink ?> "> <?php echo $key->MenuName ?></a>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><span>SAGAR</span>SHOP</a>
+				<ul class="user-menu">
+					<li class="dropdown pull-right">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> User <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
+							<li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div><!-- /.container-fluid -->
+	</nav>
+		
+	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		<ul class="nav menu">
+            <?php foreach ($this->header as $key) { ?>
+    <li><a href=" <?php echo ROOTURL. $key->MenuLink ?> "> <?php echo $key->MenuName ?></a></li>
 <?php } ?>
 <?php if (Session::get('id')): ?>
-    <a href="<?php ROOTURL?>/login/logout">Logout</a>
+    <li><a href="<?php ROOTURL?>/login/logout">Logout</a></li>
 <?php else: ?>
-    <a href="<?php ROOTURL?>/login">Login</a>
+    <li><a href="<?php ROOTURL?>/login">Login</a></li>
 <?php endif; ?>
 
 <?php if (Session::get('admin') == true): ?>
-    <a href="<?php ROOTURL?>/admin">Admin</a>
+    <li><a href="<?php ROOTURL?>/admin">Admin</a></li>
 <?php endif; ?>
 
-</div>
+		</ul>
+		<div class="attribution">admin <a href="#">rechtsboven</a></div>
+	</div><!--/.sidebar-->
+		
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">			
+		<div class="row">
+			<ol class="breadcrumb">
+				
+            <script>
+                var path = "";
+                var href = document.location.href;
+                var s = href.split("/");
+                path+='<li><a href="'+ href.substring(0,href.indexOf("/"+s[2])+s[2].length+1) +'"><span class="glyphicon glyphicon-home"></span></a></li>'
+                for (var i=3;i<(s.length-1);i++) {
+                path+="<li><a href=\""+href.substring(0,href.indexOf("/"+s[i])+s[i].length+1)+"/\">"+s[i]+"<span></span></a></li>";
+                }
+                i=s.length-1;
+                path+="<li>"+s[i]+"<span></span></li>";;
+                var url =  "" + path;
+                document.writeln(url);
+            </script>
 
-<div id="content">
-
+			</ol>
+		</div><!--/.row-->
 <?php if (isset($this->msg)){echo $this->msg;};?> <br/>
+
