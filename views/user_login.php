1f204f250568ce6c7b7f8360750e41d08e1f9f55
rjong14 <rjong14@student.avans.nl>
2019-01-15 10:25:54 +0100

    loginfix

--- views/user_login.php
+++ views/user_login.php
@@ -1,9 +1,8 @@
 <h1>loginpage</h1>
 
-<<<<<<< HEAD
+
 <h3>Login</h3>
-=======
->>>>>>> origin/master
+
 <div id="login">
 <form action="<?php ROOTURL?>/login/login" method="post">
 <label>username</label><input type="text" name="username"/> <br/>
@@ -11,12 +10,10 @@
 <label></label> <input type="submit" />
 </form>
 </div>
-<<<<<<< HEAD
+
 <br />
 <h3>Register</h3>
-=======
 
->>>>>>> origin/master
 <div id="register">
 <form action="<?php ROOTURL?>/login/register" method="post">
 <label>email-address</label><input type="text" name="emailAddress"/> <br/>
